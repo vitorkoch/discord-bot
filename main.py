12c11d96c733e81080b485e070a5b83e2550f476
Vitor Koch <vitorgabrielkock@gmail.com>
2022-05-29 14:52:23 -0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -6,6 +7,8 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
+        if message.content == 'EL GATO':
+            await message.channel.send(f':elgato:')
 
 client = MyClient()
-client.run('my token goes here')
+client.run('OTgwNTI2MTgwNDA0OTY5NTQz.GB5iqM.ST79aQw7JZ4uFWCaZH487UrflBQVmK-BbEFstI')
