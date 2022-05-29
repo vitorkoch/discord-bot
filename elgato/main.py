034a0478a72e6f1165f30a7b7e7cb0e945bbd6bb
Vitor Koch <vitorgabrielkock@gmail.com>
2022-05-29 16:09:18 -0300

    el gato

--- elgato/main.py
+++ elgato/main.py
@@ -11,6 +11,10 @@ class MyClient(discord.Client):
             await message.channel.send(f'As regras do servidor são:{os.linesep}Regra 1: Não desrespeitar os membros{os.linesep}Regra 2: blá blá blá')
         if message.content == 'el gato':
             await message.channel.send(file=discord.File('elgato.jpg'))
+        if message.content == 'if' or message.content == 'ifpa':
+            await message.channel.send('Uma merda')
+        if message.content == 'boa noite':
+            await message.channel.send(file=discord.File('images/boanoite.jpeg'))
 
 client = MyClient()
 client.run('OTgwNTI2MTgwNDA0OTY5NTQz.GG80_a.GMAjovO7yOFNQGREwe2ReBlWuqmtNFRlJFxWU4')
