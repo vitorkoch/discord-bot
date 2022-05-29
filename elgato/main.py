a3445d2591b921c8b1a50759841fbe52d428c6d0
Vitor Koch <vitorgabrielkock@gmail.com>
2022-05-29 20:26:31 -0300

    update

--- elgato/main.py
+++ elgato/main.py
@@ -3,20 +3,20 @@ import os
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print("Logged on as {0}!".format(self.user))
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-        if message.content == '!regras':
-            await message.channel.send(f'As regras do servidor são:{os.linesep}Regra 1: Não desrespeitar os membros{os.linesep}Regra 2: blá blá blá')
-        if message.content == 'el gato':
-            await message.channel.send(file=discord.File('elgato/elgato.jpg'))
-        if message.content == 'if' or message.content == 'ifpa':
-            await message.channel.send('Uma merda')
-        if message.content == 'boa noite':
-            await message.channel.send(file=discord.File('images/boanoite.jpeg'))
-        if message.content == 'el gato bebe':
-            await message.channel.send('<:elgato:977977352409722910>')
+        print("Message from {0.author}: {0.content}".format(message))
+        if message.content == "!regras":
+            await message.channel.send(f"As regras do servidor são:{os.linesep}Regra 1: Não desrespeitar os membros{os.linesep}Regra 2: blá blá blá")
+        if message.content == "el gato":
+            await message.channel.send(file=discord.File("elgato/elgato.jpg"))
+        if message.content == "if" or message.content == "ifpa":
+            await message.channel.send("Uma merda")
+        if message.content == "boa noite":
+            await message.channel.send(file=discord.File("images/boanoite.jpeg"))
+        if message.content == "el gato bebe":
+            await message.channel.send("<:elgato:977977352409722910>")
 
 client = MyClient()
-client.run('OTgwNTI2MTgwNDA0OTY5NTQz.GG80_a.GMAjovO7yOFNQGREwe2ReBlWuqmtNFRlJFxWU4')
+client.run("OTgwNTI2MTgwNDA0OTY5NTQz.GG80_a.GMAjovO7yOFNQGREwe2ReBlWuqmtNFRlJFxWU4")
