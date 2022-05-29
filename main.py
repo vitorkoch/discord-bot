64954c227441ebad889b11cc3892b0216fcc9fb9
Vitor Koch <vitorgabrielkock@gmail.com>
2022-05-29 15:57:13 -0300

    el gato

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
-        if message.content == 'EL GATO':
-            await message.channel.send(f':elgato:')
+        if message.content == '!regras':
+            await message.channel.send(f'As regras do servidor são:{os.linesep}Regra 1: Não desrespeitar os membros{os.linesep}Regra 2: blá blá blá')
+        if message.content == 'el gato':
+            await message.channel.send(file=discord.File('elgato.jpg'))
 
 client = MyClient()
-client.run('OTgwNTI2MTgwNDA0OTY5NTQz.GB5iqM.ST79aQw7JZ4uFWCaZH487UrflBQVmK-BbEFstI')
+client.run('OTgwNTI2MTgwNDA0OTY5NTQz.GG80_a.GMAjovO7yOFNQGREwe2ReBlWuqmtNFRlJFxWU4')
