4140e654359077f8d0c154a7baf142d708e2f50e
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-06-12 13:00:16 -0300

    update

--- elgato/main.py
+++ elgato/main.py
@@ -10,25 +10,25 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-        if message.content == '!regras':
+        if message.content.lower() == '!regras':
             await message.channel.send(
                 f'As regras do servidor são:{os.linesep}Regra 1: Não desrespeitar os membros{os.linesep}Regra 2: blá blá blá'
             )
-        if message.content == 'el gato':
+        if message.content.lower() == 'el gato':
             await message.channel.send(file=discord.File('elgato/elgato.jpg'))
-        if message.content == 'if' or message.content == 'ifpa':
+        if message.content.lower()== 'if' or message.content == 'ifpa':
             await message.channel.send('Destruidor de saúde mental')
-        if message.content == 'boa noite':
+        if message.content.lower() == 'boa noite':
             await message.channel.send(file=discord.File('images/boanoite.jpeg'))
-        if message.content == 'el gato bebe':
+        if message.content.lower() == 'el gato bebe':
             await message.channel.send('<:elgato:977977352409722910>')
-        if message.content == '!weka':
+        if message.content.lower() == '!weka':
             await message.channel.send(
                 f'@{message.author}, Aqui: https://docs.google.com/spreadsheets/d/1u043DjKpAY6tLJDxC7WieIYLFZZficPzeseuwvJ88j8/edit?usp=sharing'
             )
-        if message.content == 'chama todo mundo':
+        if message.content.lower() == 'chama todo mundo':
             await message.channel.send('@everyone, vem aqui')
-        if message.content == 'gay':
+        if message.content.lower() == 'gay':
             await message.channel.send(f'Tu que é gay {message.author}')
 
 
